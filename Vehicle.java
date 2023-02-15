e930b995e9ecb3d59eb95d8da02002277e445494
higginsjack <jack.higgins@colorado.edu>
2023-02-14 19:38:36 -0700

    Vehicle

--- Vehicle.java
+++ Vehicle.java
@@ -1,8 +1,69 @@
-class Vehicle {
+import java.util.Random;
+abstract class Vehicle {
     private int id;
     private String type;
+    private float repairBonus;
+    private float salesBonus;
+    private float washBonus;
+    private float cost;
+    private String condition;
+    private String cleanliness;
+
     public Vehicle(int i, String t) {
-        this.id = i;
-        this.type = t;
+        this.id = 0;
+        this.type = "Null";
+        this.repairBonus = 0;
+        this.salesBonus = 0;
+        this.washBonus = 0;
+        this.cost = 0;
+        this.condition="Null";
+        this.cleanliness="Null";
+    }
+
+    public String getType(){
+        return this.type;
+    }
+    public int getId(){
+        return this.id;
+    }
+    public float getRepairBonus(){
+        return this.repairBonus;
+    }
+    public float getSalesBonus() {
+        return this.salesBonus;
+    }
+    public float getWashBonus() {
+        return this.washBonus;
+    }
+    public float getCost(){
+        return this.cost;
+    }
+    public String getCondition() {
+        return this.condition;
+    }
+    public String getCleanliness(){
+        return this.cleanliness;
+    }
+
+    public void increaseCleanliness(){
+        Random r = new Random();
+        int high = 100;
+        int result = r.nextInt(high);
+        if(this.cleanliness == "Dirty"){
+            if(result < 10) { //10% chance of dirty
+                this.cleanliness = "Sparkling";
+            }
+            else if(result < 90) { //80% chance of clean
+                this.cleanliness = "Clean";
+            }
+        }
+        else if (this.cleanliness == "Clean") {
+            if(result < 5) {
+                this.cleanliness = "Dirty";
+            }
+            else if (result < 35) {
+                this.cleanliness = "Sparkling";
+            }
+        }
     }
 }

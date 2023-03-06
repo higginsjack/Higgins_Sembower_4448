3eda295f310963f6acf06467e19b99f9ccd7eb9e
higginsjack <jack.higgins@colorado.edu>
2023-03-06 15:45:09 -0700

    ReadMe.md update with compile/run instructions

---
+++ projectThree/Strategy.java
@@ -0,0 +1,102 @@
+public interface Strategy {
+    String washingMetod();
+ public double check(){
+     return Utility.rnd();
+ }
+ 
+ }
+ 
+ 
+ class Chemical implements Strategy{
+     public String washingMetod(){
+         return "Washing with chemical!";
+     }
+    //  @Override
+     public void check(Vehicle v, String i){
+         double brokenchance = Utility.rnd();
+         if (brokenchance <=.1){
+             v.condition = Enums.Condition.Broken;
+         }
+         double chance = Utility.rnd();
+         if (chance <= .8) {
+             v.cleanliness = Enums.Cleanliness.Clean;
+             //return  1;
+         }
+         else if  (chance >.8 && chance <=.9) {
+             v.cleanliness = Enums.Cleanliness.Sparkling;
+              b += v.wash_bonus;
+             System.out.println("Intern "+i+" got a bonus of "+Utility.asDollar(v.wash_bonus)+"!");
+             //return 2;
+         }
+ 
+         
+     }
+  }
+ 
+ 
+ class ElbowGrease implements Strategy{
+     public String washingMetod(){
+         return "Washing with elbow grease!";
+     }
+    //  @Override 
+     public void check(Vehicle v, String i){
+         double brokenchance = Utility.rnd();
+         if (brokenchance <=.1 && v.Condition != Enums.Condition.LikeNew){
+             v.condition = Enums.Condition.LikeNew;
+         }
+         if (v.condition == Enums.Condition.Dirty){//??????
+         double chance = Utility.rnd();
+         if (chance <= .7) {
+             v.Clealiness = Enums.Clealiness.Clean;
+         }
+         else if (chance >.7 && chance <=.05){
+             v.Cleanliness = Enums.Condtion.Sparkling;
+             b += v.wash_bonus;
+             System.out.println("Intern "+i+" got a bonus of "+Utility.asDollar(v.wash_bonus)+"!");
+ 
+         }
+         }
+         
+         else if (v.Condition == Enums.Condition.Clean){
+             if (chance <=.15){
+                 v.Condition = Enums.Condition.Dirty;
+             }
+             if (chance >=.85){
+                 v.Condition = V.Condition.Sparkling;
+                 b += v.wash_bonus;
+                 System.out.println("Intern "+i+" got a bonus of "+Utility.asDollar(v.wash_bonus)+"!");
+                 
+             }
+         }
+     }
+     
+ }
+ 
+ class Deatiled implements Strategy{
+     public String washingMetod(){
+         return "Washing with detailed!";
+     }
+     @override 
+     public void check(Vehicle v, String i, int b){
+         double chance = Utility.rnd();
+         if (v.Condition == Enums.Condition.Dirty){
+             if (chance <=.6){
+                 v.Condition = Enums.Condition.Clean;
+             }
+             if (chance >=.8){
+                 v.Condition = Enums.Condition.Sparkling;
+                 b += v.wash_bonus;
+             }
+         }
+         else if (v.Condition == Enums.Condition.Clean){
+             if (chance <=.05){
+                 v.Condition = Enums.Condition.Dirty;
+             }
+             if (chance >=.6){
+                 v.Condition = Enums.Condition.Sparkling;
+                 b += v.wash_bonus;
+                 System.out.println("Intern "+i+" got a bonus of "+Utility.asDollar(v.wash_bonus)+"!");
+             }
+         }
+     }
+ }
--- projectThree/readMe.md
+++ projectThree/readMe.md
@@ -2,4 +2,8 @@ Java openjdk version "17.0.6" 2023-01-17
 
 Vehicle addons add extra percentage because they don't get the original cost.
 
+javac Buyer.java dayTracker.java Enums.java FNCD.java Logger.java Main.java Namer.java Observer.java Simulator.java Staff.java Subject.java SysOut.java Tracker.java Utility.java Vehicle.java carAddOn.java RoadRescueCoverage.java SatelliteRadio.java Undercoating.java ExtendedWarranty.java
+
+java Main
+
 We appreciate the extension, this will not happen again!

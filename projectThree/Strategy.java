74c04925fba31a4f81bf22a81585940615706acc
Joseph Sembower <58923041+JosephOfTheDay@users.noreply.github.com>
2023-03-05 23:38:32 -0700

    Add files via upload

--- projectThree/Strategy.java
+++ projectThree/Strategy.java
@@ -1,5 +1,8 @@
 public interface Strategy {
    String washingMetod();
+public double check(){
+    return Utility.rnd();
+}
 
 }
 
@@ -8,11 +11,17 @@ class Chemical implements Strategy{
     public String washingMetod(){
         return "Washing with chemical!";
     }
+    @override
+        check();
+    }
 }
 
 class ElbowGrease implements Strategy{
     public String washingMetod(){
         return "Washing with elbow grease!";
+    }
+    @override 
+    check();
     }
 }
 
@@ -20,5 +29,7 @@ class Deatiled implements Strategy{
     public String washingMetod(){
         return "Washing with detailed!";
     }
+    @override 
+    check();
 }
 

a0a84c151a388e34160ab5e15bd303605fc6dc0b
Joseph Sembower <58923041+JosephOfTheDay@users.noreply.github.com>
2023-03-06 11:05:19 -0700

    Add files via upload
    changes to strategy

--- projectThree/Strategy.java
+++ projectThree/Strategy.java
@@ -12,16 +12,57 @@ class Chemical implements Strategy{
         return "Washing with chemical!";
     }
     @override
-        check();
+    public void check(Vehicle v){
+        double brokenchance = Utility.rnd();
+        if (brokenchance <=.1){
+            v.Condition = Enums.Conidtion.Broken;
+        }
+        double chance = Utility.rnd();
+        if (chance <= .8) {
+            v.cleanliness = Enums.Cleanliness.Clean;
+            //return  1;
+        }
+        else if  (chance >.8 && chance <=.9) {
+            v.cleanliness = Enums.Cleanliness.Sparkling;
+            bonusEarned += v.wash_bonus;
+            out("Intern "+name+" got a bonus of "+Utility.asDollar(v.wash_bonus)+"!");
+            out(washingMethod());
+            //return 2;
+        }
+
+        
     }
-}
+ }
+
 
 class ElbowGrease implements Strategy{
     public String washingMetod(){
         return "Washing with elbow grease!";
     }
     @override 
-    check();
+    public void check(v){
+        double brokenchance = Utility.rnd();
+        if (brokenchance <=.1 && v.Condition != Enums.Condition.LikeNew){
+            v.Condition = Enuns.Condition.LikeNew;
+        }
+        if (v.Condition == Enums.Condition.Dirty){
+        double chance = Utility.rnd();
+        if (chance <= .7) {
+            v.cleanliness = Enums.Clealiness.Clean;
+        }
+        else if (chance >.7 && chance <=.05){
+            v.Cleanliness = Enums.Condtion.Sparkling;
+        }
+        }
+        
+        else if (v.Condition == Enums.Condition.Clean){
+            if (chance <=.15){
+                v.Condition = Enums.Condition.Dirty;
+            }
+            if (chance >=.85){
+                v.Condition = V.Condition.Sparkling;
+            }
+        }
     }
 }
 

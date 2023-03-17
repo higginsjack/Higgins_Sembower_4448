cc8574f0baf9b339499e40878832b085a9ef52c7
Joseph Sembower <58923041+JosephOfTheDay@users.noreply.github.com>
2023-03-17 12:45:43 -0600

    Add files via upload
    Changed to switch statement

--- proj4/VehicleFactory.java
+++ proj4/VehicleFactory.java
@@ -1,43 +1,24 @@
 public class VehicleFactory {
     public Vehicle makeVehicle(Enums.VehicleType vType) {
         Vehicle newVehicle = null;
-
-        if (vType == Car)) {
+        switch (vType):
+        case Car:
             return new Car();
-        }
-        else 
-            if (vType == Performance){
-                return new Performance();
-            
-        }
-        else 
-            if (vType == Motorcycle){
-                return new Motorcycle();
-            }
-        else 
-            if (vType == Monster){
-                return new Monster();
-            }
-        else
-            if (vType == Pickup){
-                return new Pickup();
-            }
-        else 
-            if (vType == Electric){
-                return new Electric();
-            }
-        else 
-            if (vType == Super){
-                return new Super();
-            }
-        else
-            if (vType == F1){
-                return new F1();
-            }
-        else
-            if (vType == Hyper){
-                return new Hyper();
-            }
-        else {System.out.println("please enter a valid vehicle type");}
+        case Motorcycle:
+            return new Motorcycle();
+        case Monster:
+            return new Monster();
+        case Electric:
+            return new Electric();
+        case F1:
+            return new F1();
+        case Hyper:
+            return new Hyper();
+        case Performance:
+            return new Performance();
+        case Pickup:
+            return new Pickup();
+        default:
+            throw new IllegalArgumentException("Invalid Vehicle Type");
     }
 }

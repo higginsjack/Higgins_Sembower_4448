0e6d5d970e44d55e95aec51e1ee10308a34d1806
Joseph Sembower <58923041+JosephOfTheDay@users.noreply.github.com>
2023-03-17 18:56:28 -0600

    Add files via upload
    Factories

---
+++ Project4/StaffFactory.java
@@ -0,0 +1,19 @@
+
+public class StaffFactory {
+    public static Staff makeVehicle(Enums.StaffType sType) {
+        Staff newStaff = null;
+        switch (sType){
+        case Salesperson:
+            return new Salesperson();
+        case Intern:
+            return new Intern();
+        case Mechanic:
+            return new Mechanic();
+        case Driver:
+            return new Driver();
+        default:
+            throw new IllegalArgumentException("Invalid Staff Type");
+    }
+}
+
+}
---
+++ Project4/VehicleFactory.java
@@ -0,0 +1,27 @@
+public class VehicleFactory {
+    public static Vehicle makeVehicle(Enums.VehicleType vType) {
+        Vehicle newVehicle = null;
+        switch (vType){
+        case Car:
+            return new Car();
+        case Motorcyclce:
+            return new Motorcyclce();
+        case Monster:
+            return new Monster();
+        case Electric:
+            return new Electric();
+        case F1:
+            return new F1();
+        case Hyper:
+            return new Hyper();
+        case Performance:
+            return new Performance();
+        case Pickup:
+            return new Pickup();
+        case Super:
+            return new Super();
+        default:
+            throw new IllegalArgumentException("Invalid Vehicle Type");
+    }
+}
+}
